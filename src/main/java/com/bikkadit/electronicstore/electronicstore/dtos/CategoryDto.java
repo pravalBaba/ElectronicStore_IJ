1ebbe30f52c3094993a23ade6b13515ea6c70ac2
pravalBaba <reach2praval@gmail.com>
2023-08-05 17:19:58 +0530

    changes in Category and CategoryDto

--- src/main/java/com/bikkadit/electronicstore/electronicstore/dtos/CategoryDto.java
+++ src/main/java/com/bikkadit/electronicstore/electronicstore/dtos/CategoryDto.java
@@ -1,9 +1,6 @@
 package com.bikkadit.electronicstore.electronicstore.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Id;
@@ -15,6 +12,7 @@ import javax.validation.constraints.Size;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CategoryDto {
 
 
