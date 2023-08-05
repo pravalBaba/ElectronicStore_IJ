dc35e4c853afa760011f17d81fcf725b98d5f652
pravalBaba <reach2praval@gmail.com>
2023-08-05 14:35:11 +0530

    changes in ProductRepository

--- src/main/java/com/bikkadit/electronicstore/electronicstore/repository/ProductRepository.java
+++ src/main/java/com/bikkadit/electronicstore/electronicstore/repository/ProductRepository.java
@@ -2,14 +2,17 @@ package com.bikkadit.electronicstore.electronicstore.repository;
 
 import com.bikkadit.electronicstore.electronicstore.entities.Product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product,String> {
 
-    List<Product> findByTitleContaining(String subTitle);
+    Page<Product> findByTitleContaining(String subTitle, Pageable pageable);
 
-    List<Product> findByLiveTrue();
+    Page<Product> findByLiveTrue(Pageable pageable);
+
 
 }
